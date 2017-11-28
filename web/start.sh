34438a9a283f7406225d64beb06f7cb6defaeef0
Raül Pérez <repejota@gmail.com>
2017-11-28 06:07:14 +0100

    Added slug

--- web/start.sh
+++ web/start.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+
 # Apply database migrations
 echo "Apply database migrations"
 python3 manage.py makemigrations
+
+# Execute database migrations
+echo "Execute database migrations"
 python3 manage.py migrate
 
+# Start server
+echo "Start server"
 python3 manage.py runserver 0.0.0.0:8000
--- web/talks/models.py
+++ web/talks/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.text import slugify
 
 class Talk(models.Model):
     code = models.CharField(max_length=25, unique=True, null=False, blank=False, default=None)
     title = models.CharField(max_length=200, null=False, blank=False, default=None)
+    slug = models.SlugField(max_length=200, unique=True, blank=False)
     created = models.DateTimeField('date created')
     updated = models.DateTimeField('date updated')
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+            if Talk.objects.filter(slug=self.slug).count() > 0:
+                self.slug = self.slug + "-" + self.code
+        super(Talk, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name = "Talk"
