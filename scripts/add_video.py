2d326700d6c0cece142b01f7371f73b58acbefa4
Raül Pérez <repejota@gmail.com>
2017-12-17 04:23:21 +0100

    Refactor constants

--- scripts/add_video.py
+++ scripts/add_video.py
@@ -18,10 +18,66 @@ import requests
 from django.utils import timezone
 from django.core.wsgi import get_wsgi_application
 
+# TODO: Get the API key from an env variable/conf file/cli flag 
+YOUTUBE_API_KEY = "AIzaSyAdDZRxQSQ70JBqYXeMUGmHE1Z2evOVW4Q"
+# TODO: Use relative path
+PROJECT_PATH = "/Users/raul/Projects/vtalks/vtalks.net/web/"
+
+def do_channel(data_json):
+    """Create or update a channel.
+
+    Returns the channel object from the database.
+    """
+    from talks.models import Channel
+
+    channel_obj, created = Channel.objects.update_or_create(
+        code=data_json["snippet"]["channelId"],
+        defaults={
+            'code': data_json["snippet"]["channelId"],
+            'title': data_json["snippet"]["channelTitle"],
+            'updated': timezone.now(),
+        },
+    )
+    if created:
+        logging.info("created channel %s", channel_obj)
+    else:
+        logging.info("updated channel %s", channel_obj)
+
+    return channel_obj
+
+def do_talk(data_json, channel_obj):
+    """Create or update talk.
+    """
+    from talks.models import Talk
+
+    if "tags" not in data_json["snippet"]:
+        data_json["snippet"]["tags"] = []
+    video_obj, created = Talk.objects.update_or_create(
+        code=data_json["id"],
+        defaults={
+            'code': data_json["id"],
+            'title': data_json["snippet"]["title"],
+            'description': data_json["snippet"]["description"],
+            'channel': channel_obj,
+            'viewCount': data_json["statistics"]["viewCount"],
+            'likeCount': data_json["statistics"]["likeCount"],
+            'dislikeCount': data_json["statistics"]["dislikeCount"],
+            'tags': ", ".join(data_json["snippet"]["tags"]),
+            'updated': timezone.now(),
+        },
+    )
+    if created:
+        logging.info("created talk %s", video_obj)
+    else:
+        logging.info("updated talk %s", video_obj)
+
 def main():
     """Main entry point.
 
     Add a talk and a channel given a youtube URL.
+
+    TODO:
+    - Fetch duration from contentDetails.
     """
     # Configure logging level to INFO.
     logging.basicConfig(level=logging.INFO)
@@ -41,73 +97,29 @@ def main():
         exit(1)
     video_code = params["v"][0]
 
-    # TODO: Use relative path
-    project_path = "/Users/raul/Projects/vtalks/vtalks.net/web/"
-
-    # This is so my local_settings.py gets loaded.
-    sys.path.append(project_path)
-    os.chdir(project_path)
-
-    # This is so Django knows where to find stuff.
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
-
-    # This is so models get loaded.
-    get_wsgi_application()
-
     # Fetch video
-    youtube_api_key = "AIzaSyAdDZRxQSQ70JBqYXeMUGmHE1Z2evOVW4Q"
-
     video_url = "https://www.googleapis.com/youtube/v3/videos"
     payload = {'id': video_code,
                'part': 'snippet,statistics',
-               'key': youtube_api_key}
+               'key': YOUTUBE_API_KEY}
     resp = requests.get(video_url, params=payload)
     if resp.status_code != 200:
         logging.error(resp.status_code)
         exit(1)
-
     response_json = resp.json()
-
-    video_json = response_json["items"][0]
-
-    from talks.models import Channel
-    from talks.models import Talk
+    data_json = response_json["items"][0]
 
     # Update or create channel
-    channel_obj, created = Channel.objects.update_or_create(
-        code=video_json["snippet"]["channelId"],
-        defaults={
-            'code': video_json["snippet"]["channelId"],
-            'title': video_json["snippet"]["channelTitle"],
-            'updated': timezone.now(),
-        },
-    )
-    if created:
-        logging.info("created channel %s", channel_obj)
-    else:
-        logging.info("updated channel %s", channel_obj)
+    channel_obj = do_channel(data_json)
 
     # Update or create talk
-    if "tags" not in video_json["snippet"]:
-        video_json["snippet"]["tags"] = []
-    video_obj, created = Talk.objects.update_or_create(
-        code=video_json["id"],
-        defaults={
-            'code': video_json["id"],
-            'title': video_json["snippet"]["title"],
-            'description': video_json["snippet"]["description"],
-            'channel': channel_obj,
-            'viewCount': video_json["statistics"]["viewCount"],
-            'likeCount': video_json["statistics"]["likeCount"],
-            'dislikeCount': video_json["statistics"]["dislikeCount"],
-            'tags': ", ".join(video_json["snippet"]["tags"]),
-            'updated': timezone.now(),
-        },
-    )
-    if created:
-        logging.info("created talk %s", video_obj)
-    else:
-        logging.info("updated talk %s", video_obj)
+    do_talk(data_json, channel_obj)
 
 if __name__ == '__main__':
+    # Setup django environment & application.
+    sys.path.append(PROJECT_PATH)
+    os.chdir(PROJECT_PATH)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
+    get_wsgi_application()
+
     main()
