2595eb0aa804f672b024c8390f5facf3eee84efa
Raül Pérez <repejota@gmail.com>
2017-12-20 02:38:43 +0100

    Added corporate app to refactor views

---
+++ web/corporate/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CorporateConfig(AppConfig):
+    name = 'corporate'
+    verbose_name = 'Corporate'
--- web/talks/views.py
+++ web/talks/views.py
@@ -1,42 +1,7 @@
 from django.views.generic import TemplateView
-from django.views.generic import FormView
-
-from .forms import ContactForm
 
 # Create your views here.
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
-
-
-class AboutView(TemplateView):
-    template_name = 'about.html'
-
-
-class ContactView(FormView):
-    template_name = 'contact.html'
-    form_class = ContactForm
-    success_url = '.'
-
-    def get_context_data(self, **kwargs):
-        context = super(ContactView, self).get_context_data(**kwargs)
-        return context
-
-    def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
-        form.send_email()
-        return super(ContactView, self).form_valid(form)
-
-
-class HelpView(TemplateView):
-    template_name = 'help.html'
-
-
-class TermsView(TemplateView):
-    template_name = 'terms.html'
-
-
-class PrivacyView(TemplateView):
-    template_name = 'privacy.html'
