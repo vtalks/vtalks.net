51f86045020406eb61daac6afa2a8aad9965d4b1
Raül Pérez <repejota@gmail.com>
2017-12-22 04:11:10 +0100

    Pass a youtube_url and get its video_code

--- web/talks/management/commands/add_video.py
+++ web/talks/management/commands/add_video.py
@@ -1,11 +1,27 @@
-from django.core.management.base import BaseCommand, CommandError
+import logging
+
+from urllib.parse import urlsplit
+from urllib.parse import parse_qs
+
+from django.core.management.base import BaseCommand
+from django.core.management.base import CommandError
+
+
+def get_video_code(url):
+    query = urlsplit(url).query
+    params = parse_qs(query)
+    if "v" not in params:
+        raise CommandError('Invalid url "%s"' % url)
+    video_code = params["v"][0]
+    return video_code
 
 
 class Command(BaseCommand):
-    help = 'Adds a video to the system'
+    help = 'Adds a video to the system.'
 
     def add_arguments(self, parser):
-        parser.add_argument('youtube_url', nargs='+', type=str)
+        parser.add_argument('youtube_url', type=str)
 
     def handle(self, *args, **options):
-        print("add video!")
+        video_code = get_video_code(options['youtube_url'])
+        print(video_code)
