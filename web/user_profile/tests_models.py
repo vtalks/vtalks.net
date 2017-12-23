df50cb2f2680668551343a2269831e33a5c312ce
Raül Pérez <repejota@gmail.com>
2017-12-23 12:41:46 +0100

    More tests

---
+++ web/user_profile/tests_models.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+from django.contrib.auth.models import User
+
+from .models import UserProfile
+
+# Create your tests here.
+
+
+class UserProfileModelTests(TestCase):
+
+    def setUp(self):
+        User.objects.create_user('user_1', password='userpass')
+
+    def test_instance_get_string_repr(self):
+        """UserProfile object string representation returns its user username"""
+        user_profile_1 = UserProfile.objects.get(user__username='user_1')
+        self.assertEquals(str(user_profile_1), 'user_1')
