cd136ea335916f0052e5ef56fa4410cbf4a54a3c
Raül Pérez <repejota@gmail.com>
2017-12-23 16:24:19 +0100

    Initial form and search view

--- web/talks/views.py
+++ web/talks/views.py
@@ -1,8 +1,26 @@
 from django.views.generic import TemplateView
 
+from .models import Talk
+from .forms import SearchForm
+
 # Create your views here.
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['search_form'] = SearchForm()
+        return context
+
+
+class SearchView(TemplateView):
+    template_name = 'search.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SearchView, self).get_context_data(**kwargs)
+        context['search_form'] = SearchForm()
+        q = self.request.GET['q']
+        context['search_results'] = Talk.objects.filter(title__search=q)
+        return context
