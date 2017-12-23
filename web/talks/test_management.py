78c64c857bd445ae3a11bfb053a8c79a37db89c5
Raül Pérez <repejota@gmail.com>
2017-12-23 14:26:33 +0100

    More testing

--- web/talks/test_management.py
+++ web/talks/test_management.py
@@ -107,3 +107,75 @@ class AddVideoCommandTests(TestCase):
         self.assertIn('Adding channel "fake_channel_id"', output)
         self.assertIn('Updated channel "updated channel title"', output)
         self.assertIn('Updated talk "updated video title"', output)
+
+
+class AddPlayListCommandTests(TestCase):
+
+    @patch("talks.management.commands.add_playlist.fetch_channel_data")
+    @patch("talks.management.commands.add_playlist.fetch_video_data")
+    @patch("talks.management.commands.add_playlist.fetch_playlist_items")
+    def test_command(self, fake_fetch_playlist_items, fake_fetch_video_data, fake_fetch_channel_data):
+        # mock fetch playlist items
+        fake_fetch_playlist_items.return_value = ['fake_video_id']
+        # mock fetch video data
+        fake_fetch_video_data.return_value = {
+            "id": "fake_video_id",
+            "snippet": {
+                "channelId": "fake_channel_id",
+                "title": "video title",
+                "description": "video description",
+                "publishedAt": "2012-10-01T15:27:35.000Z",
+            },
+            "statistics": {
+                "viewCount": 20,
+            }
+        }
+        # mock fetch channel data
+        fake_fetch_channel_data.return_value = {
+            "id": "fake_channel_id",
+            "snippet": {
+                "title": "channel title",
+                "description": "channel description",
+                "publishedAt": "2012-10-01T15:27:35.000Z",
+            }
+        }
+
+        out = StringIO()
+        call_command('add_playlist', 'https://www.youtube.com/playlist?list=list_code', stdout=out)
+        output = out.getvalue()
+        self.assertIn('Added channel "channel title"', output)
+        self.assertIn('Added talk "video title"', output)
+
+        # mock fetch video data
+        fake_fetch_video_data.return_value = {
+            "id": "fake_video_id",
+            "snippet": {
+                "channelId": "fake_channel_id",
+                "title": "updated video title",
+                "description": "video description",
+                "publishedAt": "2012-10-01T15:27:35.000Z",
+                "tags": "tag1, tag2",
+            },
+            "statistics": {
+                "viewCount": 20,
+                "likeCount": 10,
+                "dislikeCount": 5,
+            }
+        }
+        # mock fetch channel data
+        fake_fetch_channel_data.return_value = {
+            "id": "fake_channel_id",
+            "snippet": {
+                "title": "updated channel title",
+                "description": "channel description",
+                "publishedAt": "2012-10-01T15:27:35.000Z",
+            }
+        }
+
+        out = StringIO()
+        call_command('add_playlist',
+                     'https://www.youtube.com/playlist?list=list_code',
+                     stdout=out)
+        output = out.getvalue()
+        self.assertIn('Updated channel "updated channel title"', output)
+        self.assertIn('Updated talk "updated video title"', output)
