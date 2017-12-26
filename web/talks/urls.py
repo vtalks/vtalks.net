dd2ea69acd9c1762b9e7893d3f0596957e5f9c72
Raül Pérez <repejota@gmail.com>
2017-12-26 02:35:35 +0100

    nice urls for latest talks and pagination

--- web/talks/urls.py
+++ web/talks/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.urls import re_path
 
 from .views import IndexView
 from .views import LatestTalks
@@ -8,5 +9,8 @@ app_name = 'talks'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('latest', LatestTalks.as_view(), name='latest-talks'),
+    re_path(r'^latest/page/(?P<page>\d+)/$', LatestTalks.as_view(), name='latest-talks-paginated'),
+    # TODO
+    # - Nice urls with search
     path('search', SearchView.as_view(), name='search'),
 ]
