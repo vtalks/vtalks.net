36c561b27de70d854de2474804f2d70eb99678c0
Raül Pérez <repejota@gmail.com>
2018-01-16 00:39:08 +0100

    better serializer for channel model listing º

--- web/talks/serializers.py
+++ web/talks/serializers.py
@@ -3,8 +3,14 @@ from rest_framework import serializers
 from .models import Channel
 
 
-class ChannelSerializer(serializers.HyperlinkedModelSerializer):
+class ChannelSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    code = serializers.CharField(required=True, allow_blank=False, max_length=100)
+    title = serializers.CharField(required=True, allow_blank=False, max_length=200)
+    slug = serializers.SlugField(read_only=True)
+    description = serializers.CharField(required=False, style={'base_template': 'textarea.html'})
+    youtube_url = serializers.URLField(read_only=True)
 
     class Meta:
         model = Channel
-        fields = ('code', 'title', 'slug', 'description', 'created', 'updated')
+        fields = ('id', 'code', 'title', 'slug', 'description', 'youtube_url')
