7b25d3cbe00b4f38d4ecfb90ff6b556625ff3985
Raül Pérez <repejota@gmail.com>
2018-01-16 03:27:44 +0100

    better serializer for Talk model

--- web/talks/serializers.py
+++ web/talks/serializers.py
@@ -18,6 +18,7 @@ class ChannelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Channel
         fields = '__all__'
+        read_only_fields = ('id', 'slug', 'youtube_url')
 
 
 class PlaylistSerializer(serializers.ModelSerializer):
@@ -31,6 +32,7 @@ class PlaylistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Playlist
         fields = '__all__'
+        read_only_fields = ('id', 'slug', 'youtube_url')
 
 
 class TalkSerializer(serializers.ModelSerializer):
@@ -39,25 +41,33 @@ class TalkSerializer(serializers.ModelSerializer):
     title = serializers.CharField(required=True, allow_blank=False, max_length=200)
     slug = serializers.SlugField(read_only=True)
     description = serializers.CharField(required=False, style={'base_template': 'textarea.html'})
+    duration = serializers.DurationField()
     youtube_url = serializers.URLField(read_only=True)
-    view_count = serializers.IntegerField(default=0, read_only=True)
-    like_count = serializers.IntegerField(default=0, read_only=True)
-    dislike_count = serializers.IntegerField(default=0, read_only=True)
-    favorite_count = serializers.IntegerField(default=0, read_only=True)
-    youtube_view_count = serializers.IntegerField(default=0, read_only=True)
-    youtube_like_count = serializers.IntegerField(default=0, read_only=True)
-    youtube_dislike_count = serializers.IntegerField(default=0, read_only=True)
-    youtube_favorite_count = serializers.IntegerField(default=0, read_only=True)
+    default_thumb = serializers.URLField(read_only=True)
+    medium_thumb = serializers.URLField(read_only=True)
+    high_thumb = serializers.URLField(read_only=True)
+    standard_thumb = serializers.URLField(read_only=True)
+    maxres_thumb = serializers.URLField(read_only=True)
+    view_count = serializers.IntegerField(default=0)
+    like_count = serializers.IntegerField(default=0)
+    dislike_count = serializers.IntegerField(default=0)
+    favorite_count = serializers.IntegerField(default=0)
+    youtube_view_count = serializers.IntegerField(default=0)
+    youtube_like_count = serializers.IntegerField(default=0)
+    youtube_dislike_count = serializers.IntegerField(default=0)
+    youtube_favorite_count = serializers.IntegerField(default=0)
     total_view_count = serializers.IntegerField(default=0, read_only=True)
     total_like_count = serializers.IntegerField(default=0, read_only=True)
     total_dislike_count = serializers.IntegerField(default=0, read_only=True)
     total_favorite_count = serializers.IntegerField(default=0, read_only=True)
-    wilsonscore_rank = serializers.FloatField(default=0, read_only=True)
-    hacker_hot = serializers.FloatField(default=0, read_only=True)
-    duration = serializers.DurationField(read_only=True)
-    created = serializers.DateTimeField(default=timezone.now, read_only=True)
-    updated = serializers.DateTimeField(default=timezone.now, read_only=True)
+    wilsonscore_rank = serializers.FloatField(default=0)
+    hacker_hot = serializers.FloatField(default=0)
+    created = serializers.DateTimeField(default=timezone.now)
+    updated = serializers.DateTimeField(default=timezone.now)
 
     class Meta:
         model = Talk
         fields = '__all__'
+        read_only_fields = ('id', 'slug', 'youtube_url',
+                            'default_thumb', 'medium_thumb', 'high_thumb', 'standard_thumb', 'maxres_thumb',
+                            'total_view_count', 'total_like_count', 'total_dislike_count', 'total_favorite_count')
