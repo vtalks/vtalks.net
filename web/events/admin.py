e115834d26347d13d1b2de9786c46f2d29b009e7
Raül Pérez <repejota@gmail.com>
2018-01-23 05:02:04 +0100

    Added new favicon

--- web/events/admin.py
+++ web/events/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from .models import Event
+
 # Register your models here.
+
+
+class EventAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'slug', 'description', 'url')
+        }),
+        ('Metadata', {
+            'classes': ('collapse',),
+            'fields': ('created', 'updated'),
+        }),
+    )
+    date_hierarchy = 'created'
+    list_filter = ['created', 'updated']
+    search_fields = ['title']
+    ordering = ['-created']
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Event, EventAdmin)
