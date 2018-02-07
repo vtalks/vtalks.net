14448bb94729a979055476961ce3dcdb345e2d76
Raül Pérez <repejota@gmail.com>
2018-02-07 01:22:13 +0100

    Small refactor on apps.py

--- web/corporate/apps.py
+++ web/corporate/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class CorporateConfig(AppConfig):
     name = 'corporate'
     verbose_name = 'Corporate'
+
+    def ready(self):
+        """Override this to put in:
+            Users system checks
+            Users signal registration
+        """
+        pass
