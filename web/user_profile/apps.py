268601997168bab90b7db0ec3802b4fb36c9aa78
Raül Pérez <repejota@gmail.com>
2018-02-23 14:39:31 +0100

    more tests

--- web/user_profile/apps.py
+++ web/user_profile/apps.py
@@ -5,11 +5,3 @@ from django.apps import AppConfig
 
 class UserProfileConfig(AppConfig):
     name = 'user_profile'
-    verbose_name = 'User Profile'
-
-    def ready(self):
-        """Override this to put in:
-            Users system checks
-            Users signal registration
-        """
-        pass
