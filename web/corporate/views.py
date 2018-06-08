72972c64a22b32d6cf31a7948b2927a017b12aa8
Raül Pérez <repejota@gmail.com>
2018-06-09 00:38:54 +0200

    sample email, it does not work!

--- web/corporate/views.py
+++ web/corporate/views.py
@@ -15,10 +15,6 @@ class ContactView(FormView):
     form_class = ContactForm
     success_url = '.'
 
-    def get_context_data(self, **kwargs):
-        context = super(ContactView, self).get_context_data(**kwargs)
-        return context
-
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
