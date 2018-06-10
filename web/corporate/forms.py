aa6e5c6eb2427010ceb645adf869c38d0a7981be
Raül Pérez <repejota@gmail.com>
2018-06-11 01:22:05 +0200

    updates

--- web/corporate/forms.py
+++ web/corporate/forms.py
@@ -10,10 +10,10 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
 
-    def send_email(self):
+    def save(self):
         request_url = 'https://api.mailgun.net/v3/mg.vtalks.net/messages'
         requests.post(request_url,
-                      auth=('api', "api:"+settings.EMAIL_HOST_PASSWORD),
+                      auth=('api', settings.EMAIL_HOST_PASSWORD),
                       data={
                           'from': 'hello@example.com',
                           'to': 'hello@vtalks.net',
