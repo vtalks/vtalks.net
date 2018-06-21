70736e85282a7fd38a7c5cf5b4111fe3d5195336
Raül Pérez <repejota@gmail.com>
2018-06-21 14:09:15 +0200

    Do not update channel when updating all videos

--- web/talks/management/commands/update_all_videos.py
+++ web/talks/management/commands/update_all_videos.py
@@ -6,9 +6,7 @@ from django.conf import settings
 from django.utils import timezone
 from django.core.management.base import BaseCommand
 
-from channels.models import Channel
 from ...models import Talk
-from youtube_data_api3.channel import fetch_channel_data
 from youtube_data_api3.video import fetch_video_data
 
 
@@ -37,32 +35,6 @@ class Command(BaseCommand):
                     talk.save()
                     exit(0)
 
-        self.stdout.write(
-            self.style.SUCCESS('Fetch talk with code "%s"' % talk_data["id"]))
-
-        channel_code = talk_data["snippet"]["channelId"]
-        channel_data = fetch_channel_data(settings.YOUTUBE_API_KEY,
-                                          channel_code)
-
-        self.stdout.write(
-            self.style.SUCCESS('Fetch channel with code "%s"' % channel_data["id"]))
-
-        # Add Channel
-        channel_obj, created = Channel.objects.update_or_create(
-            code=channel_data["id"],
-            defaults={
-                'code': channel_data["id"],
-                'title': channel_data["snippet"]["title"],
-                'description': channel_data["snippet"]["description"],
-                'created': channel_data["snippet"]["publishedAt"],
-                'updated': timezone.now(),
-            },
-        )
-        if created:
-            self.stdout.write('\tAdded channel "%s"' % channel_obj.title)
-        else:
-            self.stdout.write('\tUpdated channel "%s"' % channel_obj.title)
-
         # Add Video
         if "tags" not in talk_data["snippet"]:
             talk_data["snippet"]["tags"] = []
@@ -80,7 +52,6 @@ class Command(BaseCommand):
                 'code': talk_data["id"],
                 'title': talk_data["snippet"]["title"],
                 'description': talk_data["snippet"]["description"],
-                'channel': channel_obj,
                 'youtube_view_count': talk_data["statistics"]["viewCount"],
                 'youtube_like_count': talk_data["statistics"]["likeCount"],
                 'youtube_dislike_count': talk_data["statistics"]["dislikeCount"],
