da5b213a7d83812ef806b14935339816c997a7b1
Raül Pérez <repejota@gmail.com>
2018-06-21 02:42:02 +0200

    Initial command
    * Check youtube_url, video_code
    * Fetch data from youtube api
    * Unpublish if youtube data returns nothing
    * Unpublish if youtube upload status is failed

---
+++ web/talks/management/commands/update_video.py
@@ -0,0 +1,69 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from talks.models import Talk
+
+from youtube_data_api3.video import get_video_code
+from youtube_data_api3.video import fetch_video_data
+
+
+class Command(BaseCommand):
+    help = 'Update a video on the database.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('youtube_url_video', type=str)
+
+    def handle(self, *args, **options):
+        youtube_url_video = options['youtube_url_video']
+
+        # Get code from youtube url
+        try:
+            video_code = get_video_code(youtube_url_video)
+        except Exception:
+            print("ERROR: Invalid youtube URL video {:s}".format(youtube_url_video))
+            exit(1)
+
+        # Get talk from the database
+        try:
+            talk = Talk.objects.get(code=video_code)
+        except Talk.DoesNotExist:
+            print("ERROR: Video does not exist on the database")
+            exit(1)
+
+        print("Updating video id:{:d} - code:{:s} - youtube_url:{:s}".format(
+            talk.id,
+            video_code,
+            youtube_url_video)
+        )
+
+        # Fetch video data from Youtube API
+        youtube_video_data = fetch_video_data(settings.YOUTUBE_API_KEY, video_code)
+        # if no data is received we unpublish the video
+        if youtube_video_data is None:
+            print("Mark video unpublished because youtube API does not return data")
+            talk.published = False
+            talk.save()
+            exit(0)
+        # if status - uploadStatus on youtube is failed we unpublish the video
+        if youtube_video_data['status']['uploadStatus'] == "failed":
+            print("Mark video unpublished because youtube statusUpload is failed")
+            talk.published = False
+            talk.save()
+            exit(0)
+
+        print(youtube_video_data)
+
+        # Update video model
+
+        # Update video tags
+
+        # Update video statistics
+
+        # Recalculate sorting and ranks
+
+        # Save video to database
+
+
+
+
+
