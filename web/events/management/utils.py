9862581fa535519094135d6def8693b868e645b2
Raül Pérez <repejota@gmail.com>
2018-06-25 16:38:29 +0200

    minor tweaks

--- web/events/management/utils.py
+++ web/events/management/utils.py
@@ -2,12 +2,17 @@ import re
 
 
 def get_twitter_handle(data):
-    twitter_handle = None
+    """ Extract twitter handle
+    """
+    twitter_handle = ""
+
+    if not data:
+        return twitter_handle
 
     # Match patterns like 'https://twitter.com/handle
     regexp = "https:\/\/twitter.com\/(?P<handle>.*)"
     m = re.findall(regexp, data)
-    if len(m)>0:
+    if len(m) > 0:
         twitter_handle = m[0]
 
     return twitter_handle
