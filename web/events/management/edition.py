59035da7c605a5b70c8f11afa2b81fb1691bc296
Raül Pérez <repejota@gmail.com>
2018-06-25 18:01:32 +0200

    initial date parser

--- web/events/management/edition.py
+++ web/events/management/edition.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
 
 from events.models import Edition
+from events.management import utils
 
 
 def create_event_edition(event_json_data, year, event):
@@ -22,11 +23,18 @@ def create_event_edition(event_json_data, year, event):
     if "city" in event_json_data:
         event_edition_city = event_json_data['city']
 
+    event_edition_start = ""
+    event_edition_end = ""
+    if "date" in event_json_data:
+        event_edition_start, event_edition_end = utils.get_start_end_dates(event_json_data['date'])
+
     event_edition = Edition.objects.create(title=event_edition_name,
                                            url=event_edition_url,
                                            country=event_edition_country,
                                            city=event_edition_city,
                                            event=event,
+                                           event_start=event_edition_start,
+                                           event_end=event_edition_end,
                                            created=timezone.now())
     return event_edition
 
@@ -50,11 +58,20 @@ def update_event_edition(event_json_data, year, event):
     if "city" in event_json_data:
         event_edition_city = event_json_data['city']
 
+    event_edition_start = ""
+    event_edition_end = ""
+    if "date" in event_json_data:
+        event_edition_start, event_edition_end = utils.get_start_end_dates(event_json_data['date'])
+
+    print("----", event_edition_start, "-----", event_edition_end)
+
     event_edition = Edition.objects.get(title=event_edition_name)
     event_edition.title = event_edition_name
     event_edition.url = event_edition_url
     event_edition.country = event_edition_country
     event_edition.city = event_edition_city
     event_edition.event = event
+    event_edition.event_start = event_edition_start,
+    event_edition.event_end = event_edition_end,
 
     return event_edition
--- web/events/management/utils.py
+++ web/events/management/utils.py
@@ -16,3 +16,11 @@ def get_twitter_handle(data):
         twitter_handle = m[0]
 
     return twitter_handle
+
+
+def get_start_end_dates(data):
+    """ Extract start and end dates for an edition
+    """
+    event_edition_start = ""
+    event_edition_end = ""
+    return event_edition_start, event_edition_end
