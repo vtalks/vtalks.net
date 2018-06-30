973bc1727deb9bbf888226718d4a5a4446bab519
Raül Pérez <repejota@gmail.com>
2018-06-30 17:29:26 +0200

    cleanup of the model

--- web/talks/management/talk.py
+++ web/talks/management/talk.py
@@ -10,6 +10,76 @@ from talks.utils import parse_duration
 from youtube_data_api3.channel import get_channel_youtube_url
 
 
+def recalculate_talk_sortrank(talk):
+    """ Recalculates sort and ranking values given model's statistics
+    """
+    # wilson score
+    wilsonscore_rank = talk.get_wilson_score(talk.total_like_count, talk.total_dislike_count)
+    talk.wilsonscore_rank = wilsonscore_rank
+
+    # hacker news hot
+    votes = abs(talk.total_like_count - talk.total_dislike_count)
+    hacker_hot = talk.get_hacker_hot(votes, talk.created)
+    talk.hacker_hot = hacker_hot
+
+    return talk
+
+
+def update_talk_statistics(talk, talk_json_data):
+    """ Updates youtube video statistics ( likes, dislikes, favorites and
+    views )
+    """
+    if "statistics" in talk_json_data:
+        statistics = talk_json_data["statistics"]
+        if "viewCount" in statistics:
+            talk.youtube_view_count = statistics["viewCount"]
+        if "likeCount" in statistics:
+            talk.youtube_like_count = statistics["likeCount"]
+        if "dislikeCount" in statistics:
+            talk.youtube_dislike_count = statistics["dislikeCount"]
+        if "favoriteCount" in statistics:
+            talk.youtube_favorite_count = statistics["favoriteCount"]
+
+    return talk
+
+
+def update_talk_tags(talk, talk_json_data):
+    """ Updates tags associated to this model instance
+    """
+    tags = []
+    if "snippet" in talk_json_data:
+        snippet = talk_json_data["snippet"]
+        if "tags" in snippet:
+            tags += snippet["tags"]
+    for tag in tags:
+        talk.tags.add(tag)
+
+    return talk
+
+
+def update_talk_model(talk, talk_json_data):
+    """ Updates model's common properties
+    """
+    talk.code = talk_json_data["id"]
+    if "snippet" in talk_json_data:
+        snippet = talk_json_data["snippet"]
+        if "title" in snippet:
+            talk.title = talk_json_data["snippet"]["title"]
+        if "description" in snippet:
+            talk.description = talk_json_data["snippet"]["description"]
+        if "publishedAt" in snippet:
+            published_at = talk_json_data["snippet"]["publishedAt"]
+            datetime_published_at = datetime.strptime(published_at,"%Y-%m-%dT%H:%M:%S.000Z")
+            datetime_published_at = datetime_published_at.replace(tzinfo=timezone.utc)
+            talk.created = datetime_published_at
+    if "contentDetails" in talk_json_data:
+        content_details = talk_json_data["contentDetails"]
+        if "duration" in content_details:
+            talk.duration = parse_duration(content_details["duration"])
+
+    return talk
+
+
 def create_talk(talk_json_data):
     """ Create a new Talk into the database
     """
@@ -44,9 +114,9 @@ def create_talk(talk_json_data):
         created=talk_created,
         updated=timezone.now(),
     )
-    talk.update_video_tags(talk_json_data)
-    talk.update_video_statistics(talk_json_data)
-    talk.recalculate_video_sortrank()
+    talk = update_talk_tags(talk, talk_json_data)
+    talk = update_talk_statistics(talk, talk_json_data)
+    talk = recalculate_talk_sortrank(talk)
 
     talk.save()
 
@@ -58,10 +128,10 @@ def update_talk(talk, talk_json_data):
     """
     talk.code = talk_json_data["id"]
 
-    talk.update_video_model(talk_json_data)
-    talk.update_video_tags(talk_json_data)
-    talk.update_video_statistics(talk_json_data)
-    talk.recalculate_video_sortrank()
+    talk = update_talk_model(talk, talk_json_data)
+    talk = update_talk_tags(talk, talk_json_data)
+    talk = update_talk_statistics(talk, talk_json_data)
+    talk = recalculate_talk_sortrank(talk)
 
     if "snippet" in talk_json_data:
         snippet = talk_json_data["snippet"]
