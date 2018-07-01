9c7e3783dbfa61f9e8a31f286c7b7d9d31b6a8fb
Raül Pérez <repejota@gmail.com>
2018-07-01 16:09:08 +0200

    simple query string on search box

--- web/search/views.py
+++ web/search/views.py
@@ -1,5 +1,5 @@
 import math
-from elasticsearch import Elasticsearch
+
 
 from django.views.generic.list import ListView
 from django.conf import settings
@@ -7,48 +7,13 @@ from django.conf import settings
 from .forms import SearchForm
 
 from talks.models import Talk
+from search.search import search_talks
 
 
 class SearchTalksView(ListView):
     model = Talk
     template_name = 'search.html'
     paginate_by = settings.PAGE_SIZE
-
-    def _search_talks_elasticsearch(self, q, page=1, sort="relevance"):
-        page_start = 0
-        if page > 1:
-            page_start = self.paginate_by*(page-1)
-
-        es = Elasticsearch([{
-            'host': settings.ELASTICSEARCH['default']['HOSTNAME'],
-            'port': settings.ELASTICSEARCH['default']['PORT'],
-        }])
-
-        if sort == 'date':
-            sort = 'created'
-        elif sort == 'popularity':
-            sort = 'wilsonscore_rank'
-        else:
-            sort = '_score'
-
-        elastic_search_index = "vtalks"
-        results = es.search(index=elastic_search_index,
-                            body={
-                                "query": {
-                                    "multi_match": {
-                                        "query": q,
-                                        "fields": ["title", "description"],
-                                    },
-                                },
-                                "from": page_start,
-                                "size": self.paginate_by,
-                                "_source": ["id"],
-                                "sort": {sort: {"order": "desc"}}
-                            })
-        results_total = results['hits']['total']
-        results_ids = [ids['_id'] for ids in results['hits']['hits']]
-
-        return results_total, results_ids
 
     def get_context_data(self, **kwargs):
         context = super(SearchTalksView, self).get_context_data(**kwargs)
@@ -68,18 +33,19 @@ class SearchTalksView(ListView):
             if "sort" in self.request.GET:
                 sort = self.request.GET["sort"]
 
-            es_results_total, es_results_ids = self._search_talks_elasticsearch(q=query, page=page, sort=sort)
-            search_results = Talk.published_objects.filter(pk__in=es_results_ids)
+            results_total, results_ids = search_talks(q=query, page=page, sort=sort)
+            search_results = Talk.published_objects.filter(pk__in=results_ids)
 
-            num_pages = math.ceil(es_results_total / self.paginate_by)
-            pagination={}
-            pagination['is_paginated'] = True if es_results_total > self.paginate_by else False
-            pagination['number'] = page
-            pagination['num_pages'] = num_pages
-            pagination['has_previous'] = True if page > 1 else False
-            pagination['previous_page_number'] = page - 1
-            pagination['has_next'] = True if page < num_pages else False
-            pagination['next_page_number'] = page + 1
+            num_pages = math.ceil(results_total / self.paginate_by)
+            pagination = {
+                "is_paginated": True if results_total > self.paginate_by else False,
+                "number": page,
+                "num_pages": num_pages,
+                "has_previous": True if page > 1 else False,
+                "previous_page_number": page - 1,
+                "has_next": True if page < num_pages else False,
+                "next_page_number": page + 1,
+            }
             context['pagination'] = pagination
             context['sort'] = sort
             context['object_list'] = search_results
