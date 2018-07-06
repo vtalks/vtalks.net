9e75803ecd08192f6139618a3af952dfd6e2bce0
Raül Pérez <repejota@gmail.com>
2018-07-06 19:03:58 +0200

    typo

--- web/talks/management/commands/filter_talks.py
+++ web/talks/management/commands/filter_talks.py
@@ -18,7 +18,7 @@ class Command(BaseCommand):
 
         for talk in talks:
             event_edition_title = ""
-            if talk.event_edition.title:
+            if talk.event_edition:
                 event_edition_title = talk.event_edition.title
             out = "id: {:d} - code: {:s} - title: {:s} - edition: {:d}".format(talk.id, talk.code, talk.title, event_edition_title)
             print(out)
