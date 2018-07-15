cc036d0d46564d131cef85466f78dca7fe63bfbe
Raül Pérez <repejota@gmail.com>
2018-07-15 10:35:44 +0200

    fetch only outdated channels

--- web/channels/management/commands/update_all_channels.py
+++ web/channels/management/commands/update_all_channels.py
@@ -1,3 +1,6 @@
+from datetime import date
+from datetime import timedelta
+
 from django.core import management
 from django.core.management.base import BaseCommand
 
@@ -8,6 +11,8 @@ class Command(BaseCommand):
     help = 'Update all channels on the database.'
 
     def handle(self, *args, **options):
-        channels = Channel.objects.all().order_by("updated")
+        today = date.today()
+        yesterday = today - timedelta(days=1)
+        channels = Channel.objects.filter(updated__lte=yesterday).order_by("-updated")
         for channel in channels:
             management.call_command("update_channel", channel.youtube_url)
