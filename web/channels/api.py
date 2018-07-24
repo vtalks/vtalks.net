1421dc0a6015f0c5b6c0727c2a5f27a6a92409fd
Raül Pérez <repejota@gmail.com>
2018-07-24 08:26:34 +0200

    Filter by code

--- web/channels/api.py
+++ web/channels/api.py
@@ -4,7 +4,11 @@ from .models import Channel
 
 from .serializers import ChannelSerializer
 
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class ChannelViewSet(viewsets.ModelViewSet):
     queryset = Channel.objects.all()
     serializer_class = ChannelSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('code',)
